0766d543879889e218ff68f4d9dac3595d1f0a9a
anurrragkumar <anuragnova007@gmail.com>
2025-04-28 19:46:23 +0000

    optimized for mobile

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,14 +1,27 @@
-import React from 'react';
+import React, { useState } from 'react';
 import './Navbar.css';
 
 const Navbar = () => {
+  const [isMenuOpen, setIsMenuOpen] = useState(false);
+
+  const toggleMenu = () => {
+    setIsMenuOpen(!isMenuOpen);
+  };
+
   return (
     <nav className="navbar section">
       <div className="navbar-left">
-        <img src="https://storage.googleapis.com/sample-bucket-for-nova/profile-picture/nova.png" alt="Project Nova Logo" className="navbar-logo" />
+        <img
+          src="https://storage.googleapis.com/sample-bucket-for-nova/profile-picture/nova.png"
+          alt="Project Nova Logo"
+          className="navbar-logo"
+        />
         <h1>Project Nova</h1>
       </div>
-      <div className="navbar-right">
+      <button className="hamburger" onClick={toggleMenu}>
+        &#9776; {/* Hamburger icon */}
+      </button>
+      <div className={`navbar-right ${isMenuOpen ? 'menu-open' : ''}`}>
         <a href="#about">About</a>
         <a href="#future-plans">Future Plans</a>
         <a href="#team">Meet our Team</a>
