b47d7712cd15bc23896e67c8daac1d012d8ab670
anurrragkumar <anuragnova007@gmail.com>
2025-04-29 08:06:16 +0000

    updates role and Dhiraj picture

--- src/pages/teamSlider/TeamSlider.jsx
+++ src/pages/teamSlider/TeamSlider.jsx
@@ -6,18 +6,19 @@ const teamMembers = [
     description : `Himanshu architects and deploys the essential networking foundation in the cloud, ensuring seamless connectivity for all services. 
     He establishes secure network pathways and implements controls to protect cloud resources. His work guarantees high availability and optimal performance of the cloud environment. 
     By automating network configurations, he streamlines deployments and reduces manual effort. Ultimately, Himanshu contributes a stable, secure, and efficient network infrastructure for the organization's cloud presence.` },
-  {name: 'Lavanya Lanka', role: 'Cloud Database Engineer', image: 'https://storage.googleapis.com/sample-bucket-for-nova/profile-picture/lavanya.jpg',
-    description : `Lavanya designs and manages the critical data storage and retrieval systems in the cloud, ensuring data integrity and accessibility. 
-    She selects and optimizes database technologies to meet application needs and performance goals. Her focus on security and high availability safeguards valuable data assets.
-     Through performance tuning and efficient database management, she enables applications to operate smoothly and reliably. Lavanya contributes a robust and performant data layer that powers cloud applications.`
+  {name: 'Lavanya Lanka', role: 'Cloud DevOps Engineer', image: 'https://storage.googleapis.com/sample-bucket-for-nova/profile-picture/lavanya.jpg',
+    description : `Lavanya automates and streamlines the software delivery process in the cloud, from development to deployment. 
+    She builds and maintains CI/CD pipelines for rapid and reliable application releases. By implementing Infrastructure-as-Code,
+     she ensures consistent and scalable cloud environments. Her work in monitoring and automation enhances system stability and operational efficiency. 
+     Lavanya contributes to faster delivery cycles, improved reliability, and efficient cloud operations.`
    },
-  { name: 'Anurag Kumar', role: 'Cloud DevOps Engineer', image: 'https://storage.googleapis.com/sample-bucket-for-nova/profile-picture/anurag.jpg',
-    description : `Anurag automates and streamlines the software delivery process in the cloud, from development to deployment. 
-    He builds and maintains CI/CD pipelines for rapid and reliable application releases. By implementing Infrastructure-as-Code,
-     he ensures consistent and scalable cloud environments. His work in monitoring and automation enhances system stability and operational efficiency. 
-     Anurag contributes to faster delivery cycles, improved reliability, and efficient cloud operations.`
+  { name: 'Anurag Kumar', role: 'Cloud Database Engineer', image: 'https://storage.googleapis.com/sample-bucket-for-nova/profile-picture/anurag.jpg',
+    description : `Anurag designs and manages the critical data storage and retrieval systems in the cloud, ensuring data integrity and accessibility. 
+    He selects and optimizes database technologies to meet application needs and performance goals. His focus on security and high availability safeguards 
+    valuable data assets. Through performance tuning and efficient database management, he enables applications to operate smoothly and reliably. 
+    Anurag contributes a robust and performant data layer that powers cloud applications.`
    },
-  { name: 'Dhiraj Kumar Prince', role: 'Cloud Storage Engineer', image: '',
+  { name: 'Dhiraj Kumar Prince', role: 'Cloud Storage Engineer', image: 'https://storage.googleapis.com/sample-bucket-for-nova/profile-picture/dhiraj.jpg',
     description : `Dhiraj architects and manages the cloud storage infrastructure, ensuring data is securely stored, 
     highly available, and cost-effectively managed. He implements data protection strategies, including backups and disaster recovery. 
     His expertise in selecting and configuring storage services optimizes performance and scalability for various data needs. 
